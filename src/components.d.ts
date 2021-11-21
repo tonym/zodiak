c1f39e74e82ad10f2f954c4e41273d3248051ea4
Tony M <tony@fbpllc.com>
2021-11-20 16:15:56 -0800

    HTML reactive data example

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,17 +7,9 @@
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
     interface MyComponent {
-        /**
-          * The first name
-         */
         "first": string;
-        /**
-          * The last name
-         */
+        "fullname": string;
         "last": string;
-        /**
-          * The middle name
-         */
         "middle": string;
     }
 }
@@ -34,17 +26,9 @@ declare global {
 }
 declare namespace LocalJSX {
     interface MyComponent {
-        /**
-          * The first name
-         */
         "first"?: string;
-        /**
-          * The last name
-         */
+        "fullname"?: string;
         "last"?: string;
-        /**
-          * The middle name
-         */
         "middle"?: string;
     }
     interface IntrinsicElements {
--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,29 +1,31 @@
-import { Component, Prop, h } from '@stencil/core';
+import { Component, h, Prop, State, Watch } from '@stencil/core';
 import { format } from '../../utils/utils';
 
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.css',
-  shadow: true,
+  shadow: true
 })
 export class MyComponent {
-  /**
-   * The first name
-   */
   @Prop() first: string;
-
-  /**
-   * The middle name
-   */
   @Prop() middle: string;
-
-  /**
-   * The last name
-   */
   @Prop() last: string;
+  @Prop() fullname: string;
+  @State() fullName: { first: string; middle?: string; last: string } = { first: '', last: '' };
+
+  @Watch('fullname')
+  watchFullname(newValue: string): void {
+    if (newValue) {
+      this.fullName = typeof newValue === 'object' ? newValue : JSON.parse(newValue);
+    }
+  }
+
+  componentWillLoad(): void {
+    this.watchFullname(this.fullname);
+  }
 
   private getText(): string {
-    return format(this.first, this.middle, this.last);
+    return format(this.fullName.first, this.fullName.middle, this.fullName.last);
   }
 
   render() {
